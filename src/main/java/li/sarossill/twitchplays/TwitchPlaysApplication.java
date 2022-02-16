3ed042c3eaf49d01acfad2291b43cd1c7b8c96e7
Sam Rossilli <Sarossilli@gmail.com>
2022-02-15 19:32:32 -0700

    Initial commit

---
+++ src/main/java/li/sarossill/twitchplays/TwitchPlaysApplication.java
@@ -0,0 +1,13 @@
+package li.sarossill.twitchplays;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitchPlaysApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TwitchPlaysApplication.class, args);
+    }
+
+}
---
+++ src/test/java/li/sarossill/twitchplays/TwitchPlaysApplicationTests.java
@@ -0,0 +1,13 @@
+package li.sarossill.twitchplays;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwitchPlaysApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
